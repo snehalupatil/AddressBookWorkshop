d77628a185d909f85783bfb1bf66234d7f43c0d9
snehalupatil <patilsnehal4575@gmail.com>
2021-06-03 11:41:57 +0530

    Added: Welcome message is added

---
+++ main/java/AddressBook.java
@@ -0,0 +1,8 @@
+public class AddressBook {
+    public static void main(String[] args) {
+        /**
+         * Added welcome message
+         */
+        System.out.println("Welcome to AddressBook System");
+    }
+}
